c6a4bdcf7fbf56fbb1754a9ffdaecf7eedcceabb
unknown <vanuvalentina16@gmail.com>
2020-07-31 19:01:24 +0300

    COnnect/Skills

---
+++ web/_asset/font/my-cv-v2-starter_icons.dart
@@ -0,0 +1,33 @@
+// Place fonts/my-cv-v2-starter.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: my-cv-v2-starter
+//      fonts:
+//       - asset: fonts/my-cv-v2-starter.ttf
+import 'package:flutter/widgets.dart';
+
+class My_cv_v2_starter {
+  My_cv_v2_starter._();
+
+  static const String _fontFamily = 'my-cv-v2-starter';
+
+  static const IconData arrow_down = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData arrow_up = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData book = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData codetap = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData codewars = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData download = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData facebook = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData heart = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData info = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData instagram = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData linkedin = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData skype = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData stackoverflow = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData trophy = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData twitter = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData udemy = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData youtube = IconData(0xe914, fontFamily: _fontFamily);
+}
